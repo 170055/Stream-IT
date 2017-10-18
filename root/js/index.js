eb463aeeec265925fefdc5265808cd24968e75a7
Leo kuyper <leokuyper@gmail.com>
2017-10-18 23:25:16 +0200

    Home Page Mostly fixed
    Scaling has been improved. Image scaling has been fixed.
    
    As well as a basic credential system has been added, it needs authentication to continue to the Home Page.
    
    Username: Mike
    Password: rocks

--- root/js/index.js
+++ root/js/index.js
@@ -1,5 +1,22 @@
 $(function(){
     console.log("Ready");
 
+      $("#submit").on("click", function(event){
+          event.preventDefault();
+
+          var nameInput = $("#user-input").val();
+          var passInput = $("#password-input").val();
+
+
+          if ( nameInput == "Mike"){
+            if (passInput == "rocks"){
+              $(location).attr('href', 'pages/home.html');
+            }
+          } else {
+            console.log("Username or Password wrong");
+          }
+
+
+      });
 
 });
