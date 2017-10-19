5bd69d40002c91662f3616594d83c11b676794e3
170071 <170071@virtualwindow.co.za>
2017-10-19 10:14:17 +0200

    Password Update

--- root/js/index.js
+++ root/js/index.js
@@ -15,6 +15,46 @@ $(function(){
           } else {
             console.log("Username or Password wrong");
           }
+          
+          if ( nameInput == "Mike"){
+            if (passInput == "rocks"){
+              $(location).attr('href', 'pages/home.html');
+            }
+          } else {
+            console.log("Username or Password wrong");
+          }
+          
+          if ( nameInput == "Leo"){
+            if (passInput == "rocks"){
+              $(location).attr('href', 'pages/home.html');
+            }
+          } else {
+            console.log("Username or Password wrong");
+          }
+          
+          if ( nameInput == "Michael"){
+            if (passInput == "rocks"){
+              $(location).attr('href', 'pages/home.html');
+            }
+          } else {
+            console.log("Username or Password wrong");
+          }
+          
+          if ( nameInput == "Andrea Wilkins"){
+            if (passInput == "Kyla"){
+              $(location).attr('href', 'pages/home.html');
+            }
+          } else {
+            console.log("Username or Password wrong");
+          }
+          
+          if ( nameInput == "Kyla McGee"){
+            if (passInput == "Andrea"){
+              $(location).attr('href', 'pages/home.html');
+            }
+          } else {
+            console.log("Username or Password wrong");
+          }
 
 
       });
