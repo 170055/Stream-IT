99bfe4399d1d7bc0649445bc039040483370ac93
170071 <170071@virtualwindow.co.za>
2017-11-08 22:04:22 +0200

    OMDB filter Phase 1

--- root/js/main.js
+++ root/js/main.js
@@ -1,43 +1,116 @@
-var movies=["Star Wars", "Thor: Ragnarok", "Arrival", "Harry potter", "Titanic", "Baby driver", "insurgent", "La la land", "Jigsaw",  "It", "Blade Runner 2049", "Murder on the Orient Express",  "Geostorm", "Happy Death Day", "A Bad Moms Christmas", "Kingsman: The Golden Circle", "Atomic Blonde", " Suburbicon", "1922", "La la land", "Spider-Man Homecoming", "Jungle", "The Snowman"];
+var movies = ["Star Wars", "Thor: Ragnarok", "Arrival", "Harry potter", "Titanic", "Baby driver", "insurgent", "La la land", "Jigsaw", "It", "Blade Runner 2049", "Murder on the Orient Express", "Geostorm", "Happy Death Day", "A Bad Moms Christmas", "Kingsman: The Golden Circle", "Atomic Blonde", " Suburbicon", "1922", "La la land", "Spider-Man Homecoming", "Jungle", "The Snowman"];
 //Movies needs to be added/replaced
 
-for (var i = 0; i < 8; i++) {
+function main() {
+  for (var i = 0; i < 8; i++) {
+    $("#con"+ i).css("background-image", 'url(' + '/' + ')');
 
-  $.getJSON('https://www.omdbapi.com/?t='+ encodeURI(movies[i])+ '&apikey=90d22851').then(function(response){
-      console.log(response);
-      var title = response.Title;
-      var image = response.Poster;
-      var genre = response.genre;
-      var imdb = response.imdbRating;
-      var imdbVote = response.imdbVotes;
-      var year = response.Year;
-      var rated = response.Rated;
-      var plot = response.Plot;
+  }
+
+  $.getJSON('https://www.omdbapi.com/?t=' + encodeURI(movies[1]) + '&apikey=90d22851').then(function(response) {
 
 
 
+    var title = response.Title;
+    var image = response.Poster;
+    var genre = response.Genre;
+    var imdb = response.imdbRating;
+    var imdbVote = response.imdbVotes;
+    var year = response.Year;
+    var rated = response.Rated;
+    var plot = response.Plot;
+
+    var check = true;
+    var genresl = $("#genre").val();
 
 
-    if(image !== "N/A"){
+    //genre filter
+    if ($("#genre").val() == "all") {
+      if(check == true){
+        check = true;
+      }
+    }else {
+      check = false;
 
-      $("#con"+ 1).css("background-image", 'url(' + image + ')');
-      $("#con"+ 2).css("background-image", 'url(' + image + ')');
-      $("#con"+ 3).css("background-image", 'url(' + image + ')');
-      $("#con"+ 4).css("background-image", 'url(' + image + ')');
-      $("#con"+ 5).css("background-image", 'url(' + image + ')');
-      $("#con"+ 6).css("background-image", 'url(' + image + ')');
-      $("#con"+ 7).css("background-image", 'url(' + image + ')');
-      $("#con"+ 0).css("background-image", 'url(' + image + ')');
-      $("#con"+ 1).css("background-image", 'url(' + image + ')');
+      if (genre.indexOf(genresl) != -1) {
+        console.log(genresl + " found");
+
+        check = true;
+
+      }else {
+        check = false;
+      }
 
     }
 
+    //genre filter
 
-    });
+    console.log(check+ " 1");
+
+    if ($("#imdb").val() == "all") {
+      if(check == true){
+        check = true;
+      }
+    }else {
+      check = false;
+    }
+
+    if ($("#userrating").val() == "all") {
+      if(check == true){
+        check = true;
+      }
+    }else {
+      check = false;
+    }
+
+    if ($("#year").val() == "all") {
+      if(check == true){
+        check = true;
+      }
+    }else {
+      check = false;
+    }
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+    if(check === true){
+    if(image !== "N/A"){
+
+     $("#con"+ 1).css("background-image", 'url(' + image + ')');
+
+    }
+}
+
+  });
+
+
+
+
+  //}
 
 }
+
+$(function() {
+
+  $(".datainput").on('change input', function() {
+    main();
+  });
+
+});
+
+
+main();
