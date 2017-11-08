3e9ebbab08312717a898fbb559d9e7f1d845b092
170071 <170071@virtualwindow.co.za>
2017-11-08 15:23:09 +0200

    OMDB Stage 1

--- root/js/main.js
+++ root/js/main.js
@@ -1,42 +1,23 @@
-function omdbCall() {
-
-
-  var counter=0;
-
-
-
-  for (var i = 0; i < 8; i++) {
-    var movies = ["Star Wars", "Game of Thrones"];
-    var movie = movies[counter];
-    console.log(counter+" test");
-
-
-
-  $.getJSON('https://www.omdbapi.com/?t='+ encodeURI(movie)+ '&apikey=90d22851').then(function(response){
-      var image = response.Poster;
+function omdbCall(title, pos) {
+  $.getJSON('https://www.omdbapi.com/?t='+ encodeURI(title)+ '&apikey=90d22851').then(function(response){
       console.log(response);
-
-
-
-        console.log(i);
+      var image = response.Poster;
 
         if(image !== "N/A"){
 
-          $("#con"+1).css("background-image", 'url(' + image + ')');
+          $("#con"+ pos).css("background-image", 'url(' + image + ')');
 
         }
-
-
-
-
-
   });
 
-counter++;
+}
 
+var movies=["Star Wars", "Thor: Ragnarok", "Arrival", "Harry potter", "Titanic", "Baby driver", "insurgent", "La la land", "Jigsaw",  "It", "Blade Runner 2049", "Murder on the Orient Express",  "Geostorm", "Happy Death Day", "A Bad Moms Christmas", "Kingsman: The Golden Circle", "Atomic Blonde", " Suburbicon", "1922", "La la land", "Spider-Man Homecoming", "Jungle", "The Snowman"]; //Movies needs to be added/replaced
+
+for (var i = 0; i < 8; i++) {
+
+
+    omdbCall(movies[i+14], i);
 
 
 }
-}
-
-omdbCall();
