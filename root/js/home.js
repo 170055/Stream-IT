9b32d35225428fe65c50972d7e676bcd3e1aba5e
AndreaWilkins170085 <32729395+AndreaWilkins170085@users.noreply.github.com>
2017-11-09 11:51:42 +0200

    Home page complete
    Bootstrapping improved and search modal added

--- root/js/home.js
+++ root/js/home.js
@@ -4,4 +4,14 @@ $(function (){
     $(window).load(function(){        
        $('#myModal').modal('show');
      }); 
+    
+    $("#searchButton").click(function(event){ 
+        event.preventDefault();
+       $('#searchModal').modal('show');
+     });
+    
 });
+
+// search bar modal 
+
+     
