6d4285779fe52aac97d9fb40f6ea5e22f8fb0f61
170071 <170071@virtualwindow.co.za>
2017-11-15 12:42:36 +0200

    Update all pages

--- root/js/main.js
+++ root/js/main.js
@@ -1,9 +1,13 @@
+<<<<<<< HEAD
+var movies = ["Insurgent", "Alien: Covenant", "John Wick", "Harry potter",
+=======
 $.ajax({
   type: 'POST',
   async: false,
 });
 
 var movies = ["Insurgent", "Get out", "John Wick", "Harry potter",
+>>>>>>> 273d3f6377550f8c62acf7ec4338afdd14d590d8
  "Titanic", "Baby driver", "Hunger Games",  "Jigsaw", "It", "Blade Runner 2049", "Murder on the Orient Express", "Geostorm", "Happy Death Day", "A Bad Moms Christmas", "Kingsman: The Golden Circle", "Atomic Blonde", " Suburbicon",  "Spider-Man Homecoming", "Jungle","Star Wars" , "Flatliners", "The maze runner", "Grease", "Wonder woman",
   "Dunkirk", "Logan", "Thor", "Insidious", "Kong: Skull island", "Annabelle", "Baywatch", "The mummy", "Bad moms", "Mr Bean", "Justice league", "The lego movie", "Love and other drugs"];
 //Movies needs to be added/replaced
@@ -30,22 +34,21 @@ var plot = '';
 
 function main() {
 
-  console.log(counter);
-  console.log(con);
+  console.log(counter + " counter");
+  console.log(con + " con");
 
   if($(document).find("title").text() == "Watch List"){
-      content = movies[counter];
+
   }
 
-    if($(document).find("title").text() == "Watch List"){
-        content = movies[counter];
-    }
+content = movies[counter]; //move later
 
   $.ajax({
     url: 'https://www.omdbapi.com/?t=' + encodeURI(content) + '&apikey=90d22851',
     dataType: "json",
     async: false
   }).done(function(response) {
+
       image = response.Poster;
       title = response.Title;
       genre = response.Genre;
@@ -82,7 +85,7 @@ function main() {
     if(all === true){
 
       //add content
-      $("#grey-con").append("<div id=" + "con"+counter + " class='datadisplay col-xs-10 col-sm-5 col-md-3 col-lg-3'></div>");
+      $("#grey-con").append("<div id=" + "con"+con + " class='datadisplay col-xs-10 col-sm-5 col-md-3 col-lg-3'></div>");
 
       $("#con"+ counter).append("<div class='overlay'> <div class='movie-content'> <div class='play-btn'></div> <div class='more-info'></div> <div class='addwatchlist'></div> </div> </div>");
 
@@ -90,7 +93,39 @@ function main() {
 
       $("#con"+ counter).css("background-image", 'url(' + image + ')');
 
+    }else {
+      var check = true;
+
+    var genresl = $("#genre").val();
+
+    //year
+    if (year >=! $("#year").val()){
+        check = false;
     }
+    //year
+
+    //genre filter
+   if ($("#genre").val() == "all") {
+     if(check == true){
+       check = true;
+     }
+   }else {
+     if (genre.indexOf(genresl) != -1) {
+       check = true;
+     }else {
+       check = false;
+     }
+
+
+   }
+
+   //genre filter
+
+    console.log(check + " checked");
+
+
+    }
+
 
     });
 
@@ -113,9 +148,24 @@ $(function (){
   var i=0;
 
   while (i < 8){
+<<<<<<< HEAD
+=======
     console.log(counter);
 
+<<<<<<< HEAD
+main();
 
+
+$(function (){ 
+// search bar modal
+    $("#searchButton").click(function(event){ 
+        event.preventDefault();
+       $('#searchModal').modal('show');
+     });
+    
+=======
+
+>>>>>>> 887799d57e8b2505f941c3019ea44f9adfa63395
     main();
     counter++;
     con++;
@@ -125,4 +175,5 @@ $(function (){
 
 
 
+>>>>>>> 273d3f6377550f8c62acf7ec4338afdd14d590d8
 });
