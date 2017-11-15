273d3f6377550f8c62acf7ec4338afdd14d590d8
170071 <170071@virtualwindow.co.za>
2017-11-15 12:27:14 +0200

    Ajaxa implemented

--- root/js/main.js
+++ root/js/main.js
@@ -1,3 +1,8 @@
+$.ajax({
+  type: 'POST',
+  async: false,
+});
+
 var movies = ["Insurgent", "Get out", "John Wick", "Harry potter",
  "Titanic", "Baby driver", "Hunger Games",  "Jigsaw", "It", "Blade Runner 2049", "Murder on the Orient Express", "Geostorm", "Happy Death Day", "A Bad Moms Christmas", "Kingsman: The Golden Circle", "Atomic Blonde", " Suburbicon",  "Spider-Man Homecoming", "Jungle","Star Wars" , "Flatliners", "The maze runner", "Grease", "Wonder woman",
   "Dunkirk", "Logan", "Thor", "Insidious", "Kong: Skull island", "Annabelle", "Baywatch", "The mummy", "Bad moms", "Mr Bean", "Justice league", "The lego movie", "Love and other drugs"];
@@ -8,143 +13,116 @@ var series = ["Star Wars", "Thor: Ragnarok"];
 
 
 var counter = 0;
+var con = 0;
 var move = 0;
+var content;
+
+var title = '';
+var image = '';
+var genre = '';
+var imdb = '';
+var imdbVote = '';
+var year = '';
+var rated = '';
+var plot = '';
 
 
 
 function main() {
+
+  console.log(counter);
+  console.log(con);
+
+  if($(document).find("title").text() == "Watch List"){
+      content = movies[counter];
+  }
+
+    if($(document).find("title").text() == "Watch List"){
+        content = movies[counter];
+    }
+
+  $.ajax({
+    url: 'https://www.omdbapi.com/?t=' + encodeURI(content) + '&apikey=90d22851',
+    dataType: "json",
+    async: false
+  }).done(function(response) {
+      image = response.Poster;
+      title = response.Title;
+      genre = response.Genre;
+      imdb = response.imdbRating;
+      imdbVote = response.imdbVotes;
+      year = response.Year;
+      rated = response.Rated;
+      plot = response.Plot;
+
+
+      console.log(title+"1");
+    }).then(function() {
+
+      console.log("Title:" + title);
+      console.log("Image:" + title);
+
+        var all = true;
+
+        if ($("#imdb").val() !== "all") {
+          all = false;
+        }
+        if ($("#genre").val() !== "all") {
+          all = false;
+        }
+        if ($("#userrating").val() !== "all") {
+          all = false;
+        }
+        if ($("#year").val() !== "all") {
+          all = false;
+        }
+
+        console.log(all);
+
+    if(all === true){
+
+      //add content
+      $("#grey-con").append("<div id=" + "con"+counter + " class='datadisplay col-xs-10 col-sm-5 col-md-3 col-lg-3'></div>");
+
+      $("#con"+ counter).append("<div class='overlay'> <div class='movie-content'> <div class='play-btn'></div> <div class='more-info'></div> <div class='addwatchlist'></div> </div> </div>");
+
+      //add content
+
+      $("#con"+ counter).css("background-image", 'url(' + image + ')');
+
+    }
+
+    });
+
+}
+
+function clear() {
   //clear
-  for (var i = 0; i < 8; i++) {
-    $("#con"+ i).css("background-image", 'url(' + '/' + ')');
+  $('#grey-con').empty();
+}
+
+$(function(){ $(".datainput").on('change input', function(){
+    clear();
+    main();
+
+  });
+});
+
+$(function (){
+  clear();
+  var i=0;
+
+  while (i < 8){
+    console.log(counter);
+
+
+    main();
+    counter++;
+    con++;
+    i++;
 
   }
 
-for (var i = 0; i < 8; i++) {
 
-
-  (function(i) {
-
-    if($(document).find("title").text() == "Movies"){
-
-    }
-
-  $.getJSON('https://www.omdbapi.com/?t=' + encodeURI(movies[i + move]) + '&apikey=90d22851').then(function(response) {
-
-
-    console.log(counter);
-    console.log(move +" move");
-
-    var title = response.Title;
-    var image = response.Poster;
-    var genre = response.Genre;
-    var imdb = response.imdbRating;
-    var imdbVote = response.imdbVotes;
-    var year = response.Year;
-    var rated = response.Rated;
-    var plot = response.Plot;
-
-
-    var check = true;
-
-    var genresl = $("#genre").val();
-    console.log(response);
-    //year
-    if ($("#year").val() == "all") {
-      if(check == true){
-        check = true;
-      }
-    }else {
-      if (year >= $("#year").val()){
-
-        check = true;
-      }else {
-        check = false;
-      }
-    }
-    //year
-
-    //genre filter
-    if ($("#genre").val() == "all") {
-      if(check == true){
-        check = true;
-      }
-    }else {
-      if (genre.indexOf(genresl) != -1) {
-        check = true;
-      }else {
-        check = false;
-      }
-
-
-    }
-
-    //genre filter
-
-
-
-
-    console.log(check+ " 1");
-    //imdb
-
-    if ($("#imdb").val() == "all") {
-      if(check == true){
-        check = true;
-      }
-    }else {
-      if ($("#imdb").val() <= imdb-1){
-        check = false;
-      }else {
-        if (check == false) {
-          check = false;
-        }else {
-          check = true;
-        }
-      }
-    }
-    console.log(imdb);
-    //imdb
-    console.log(check+ " 2");
-
-    //userrating ask Mike!!!!
-    if ($("#userrating").val() == "all") {
-      if(check == true){
-        check = true;
-      }
-    }else {
-      if ($("#userrating").val() <= imdb-1){
-        check = true;
-      }else {
-        check = false;
-      }
-    }
-    //userrating ask Mike!!!!
-    console.log(check+ " 3");
-    console.log(check+ " 4");
-    if(check === true){
-     $("#con"+ counter).css("background-image", 'url(' + image + ')');
-     console.log("works");
-     counter++;
-}else {
-  i = i-1;
-  move++;
-}
-  console.log(i+move+ " data");
-  console.log(move +" move");
-  console.log(movies.length + " movies");
-  console.log(i +" i");
-  console.log(counter + " counter");
-  });
-})(i);
-}
-}
-$(function() {
-  $(".datainput").on('change input', function() {
-    counter=0;
-    move=0;
-    main();
-  });
 
 });
-
-
-main();
