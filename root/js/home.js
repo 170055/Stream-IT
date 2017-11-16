7afa9c345073a514e5ae9c8bb94f957c1119552f
170071 <170071@virtualwindow.co.za>
2017-11-16 09:34:49 +0200

    Final

--- root/js/home.js
+++ root/js/home.js
@@ -1,27 +1,33 @@
 //Modals
 
-$(function (){ 
+$(function (){
 // on load modal
-    $(window).load(function(){        
+    $(window).load(function(){
        $('#myModal').modal('show');
-     }); 
- 
+     });
+
 // search bar modal
-    $("#searchButton").click(function(event){ 
+    $("#searchButton").click(function(event){
         event.preventDefault();
        $('#searchModal').modal('show');
      });
-    
+
 // watch modal
-    $("#watchn").click(function(event){ 
+    $("#watchn").click(function(event){
         event.preventDefault();
        $('#Watch-Modal').modal('show');
      });
-    
+
+     $(document).on('click', '#plist', function(event) {
+
+       event.preventDefault();
+
+
+
+       $('#noti-modal').modal('show');
+
+     });
+
 });
 
 //slide show
-
-
-
-     
